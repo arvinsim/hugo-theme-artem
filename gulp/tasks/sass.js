b8b5c7f8de54e871d9864b1a6c20ce74ed156b5e
arvinsim <arthurvincent.simon@gmail.com>
2015-02-23 21:29:55 +0800

    Added autoprefixer for gulp. Added dev task for gulp.

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var config = require('../config').sass
@@ -6,7 +7,12 @@ var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
     gulp.src(config.src)
+        .pipe(plumber())
         .pipe(sourcemaps.init())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(sass(config.settings))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(config.dest))
